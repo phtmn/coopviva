ae44dc7fc3e73966a837b4c4a16fe5d375350e9c
Diego Paiva <diegopaiva04@gmail.com>
2018-12-28 22:23:04 -0200

    Andamento Textos

--- resources/views/site/master.blade.php
+++ resources/views/site/master.blade.php
@@ -32,17 +32,17 @@
                 <div class="row flex-wrap justify-content-center justify-content-lg-between align-items-lg-center">
                     <div class="col-12 col-lg-8 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
                         <div class="header-bar-email">
-                            E-mail: <a href="#">contato@coopviva.com.br</a>
+                            E-mail: <a href="#">contato@redeconexao.art.br</a>
                         </div><!-- .header-bar-email -->
 
                         <div class="header-bar-text">
-                            <p>Telefones: <span>+24 3772 120 091 / +56452 4567</span></p>
+                            <p>Telefones: <span>+55 83 3034-1663</span></p>
                         </div><!-- .header-bar-text -->
                     </div><!-- .col -->
 
                     <div class="col-12 col-lg-4 d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">
                         <div class="donate-btn">
-                            <a href="#">Doe Agora</a>
+                            <a href="#">Invista Agora</a>
                         </div><!-- .donate-btn -->
                     </div><!-- .col -->
                 </div><!-- .row -->
@@ -54,17 +54,16 @@
                 <div class="row">
                     <div class="col-12 d-flex flex-wrap justify-content-between align-items-center">
                         <div class="site-branding d-flex align-items-center">
-                           <a class="d-block" href="index.html" rel="home"><img class="d-block" src="{{asset('imgs/coopvidapreta_logo.png')}}" alt="logo"></a>
+                           <a class="d-block" href="index.html" rel="home"><img class="d-block" src="{{asset('imgs/logo-branco.png')}}" alt="logo"></a>
                         </div><!-- .site-branding -->
 
                         <nav class="site-navigation d-flex justify-content-end align-items-center">
                             <ul class="d-flex flex-column flex-lg-row justify-content-lg-end align-content-center">
-                                <li class="current-menu-item"><a href="index.html">Inicio</a></li>
+                                <li class="current-menu-item"><a href="index.html">Home</a></li>
                                 <li><a href="#">Sobre Nos</a></li>
-                                <li><a href="#">Causas</a></li>
-                                <li><a href="#">Galeria</a></li>
+                                <li><a href="#">OSC's</a></li>
+                                <li><a href="#">Agenda 2030</a></li>
                                 <li><a href="#">Blog</a></li>
-                                <li><a href="#">Contato</a></li>
                                 @if (Route::has('login'))               
                                     @auth
                                         <li><a href="{{ url('/home') }}">Home</a></li>
