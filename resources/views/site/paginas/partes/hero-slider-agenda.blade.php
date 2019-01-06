a11eb0f6eb1c5909642c66d1c29ee9d3c85f0d36
Pedro Henrique Tolentino de Melo Nogueira <pedro.nogueira@hotmail.com.br>
2019-01-06 00:47:53 -0300

    teste

---
+++ resources/views/site/paginas/partes/hero-slider-agenda.blade.php
@@ -0,0 +1,23 @@
+<div class="swiper-container ">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide hero-content-wrap">
+                <img src="/vendor/site/images/2agenda2030.png" alt="">
+
+                <div class="hero-content-overlay position-absolute w-100 h-100">
+                    <div class="container h-100">
+                        <div class="row h-100">
+                            <div class="col-12 col-lg-10 d-flex flex-column justify-content-center align-items-start">
+                                <header class="entry-header">
+                                    <h4>Agenda 2030</h4>
+                                    
+                                </header><!-- .entry-header -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div><!-- .hero-content-overlay -->
+            </div><!-- .hero-content-wrap -->
+    </div><!-- .hero-slider -->
+
+
+
+    
