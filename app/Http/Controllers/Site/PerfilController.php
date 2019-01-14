5ad78c9b8f31031d115ccf136f727c617ca92981
Diego Paiva <diegopaiva04@gmail.com>
2019-01-14 20:22:48 -0200

    validation perfilcontroller

--- app/Http/Controllers/Site/PerfilController.php
+++ app/Http/Controllers/Site/PerfilController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Perfil;
 use Auth;
+use Validator;
 
 class PerfilController extends Controller
 {
@@ -39,8 +40,38 @@ class PerfilController extends Controller
         $perfil->estado = $request['estado'];
         $perfil->complemento = $request['complemento'];
         //TODO: inserir os campos relacionado a perfil
+        //validação
+        $messages = [
+            'required'  => 'O campo :attribute é obrigatório', 
+            'min'       => 'A quantidade de caracteres do campo :attribute é minima', 
+            'max'       => 'A quantidade de caracteres do campo :attribute é maxima', 
+            'string'    => 'O campo :attribute não é do tipo texto',
+            'unique'    => 'O valor do campo :attribute já está em uso, escolha outro'
+        ];
 
+        $validator = Validator::make($request->all(),[  
+            'nome_completo'         => 'required|min:10|max:100|string|',
+            'data_nascimento'       => 'required|min:8',
+            'telefone_principal'    => 'required|min:14|max:15',
+            'cpf'                   => 'required|unique|max:11',
+            'cnpj'                  => 'required|unique|size:18',
+            'razao_social'          => 'required|string|min:4|max:100',
+            'nome_fantasia'         => 'required|string|min:4|max:100',
+            'cep'                   => 'required|max:8',
+            'endereco'              => 'required|min:4|max:100',
+            'cidade'                => 'required|min:4|max:100',
+            'estado'                => 'required|min:4|max:100',
+            'complemento'           => 'required|min:2|max:100'                   
+        ],$messages);
+        
+        //caso alguma regra falhe
+        if($validator->fails()){
 
+            return redirect()->back()       // = volta pra pagina anterior
+                            ->withErrors($validator) // retorna os erros de validação
+                            ->withInput(); // retorna com os campos preenchidos
+        }
+        
         //grava os dados no banco
         $perfil->save();
         return redirect()->route('site.painel');
