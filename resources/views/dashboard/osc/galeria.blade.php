cd377bd4547520bfdf94e4e436db2958e5464c8c
Rafael <rafael.andradejppb@gmail.com>
2019-02-07 19:23:39 -0200

    alteraçoes na galeria

--- resources/views/dashboard/osc/galeria.blade.php
+++ resources/views/dashboard/osc/galeria.blade.php
@@ -7,24 +7,26 @@
 
     <div class="container">
         <div class="form-group" style="margin-top: 25px">
-            <a href="{{ route('galeria.create') }}" class="btn btn-primary">Incluir foto</a>
+            <a href="{{ route('galeria.create') }}" class="btn gradient-bg">Incluir foto</a>
         </div>
+        <div class="jumbotron jumbotrn-fluid">
         <h2>Galerias de Imagens</h2>
-        <div class="portfolio-wrap">
+        <!--<div class="portfolio-wrap">-->
             <div class="container">
-                <div class="row portfolio-container">
+                <div class="row ">
                     @forelse($galerias as $g)
-                    <div class="col-12 col-md-6 col-lg-4 portfolio-item">
+                    <div class="col-xs-12 col-md-6 col-lg-4 portfolio-item">
                         <div class="portfolio-cont">
-                            <a href="#"><img src="{{asset($g->url)}}" alt=""></a>
+                            <a href="#"><img src="{{asset($g->url)}}" alt="" class="img img-responsive" style="width:300px; height:300px; margin-left:-4px"></a>
                             <h3 class="entry-title"><a href="#">{{$g->descricao}}</a></h3>
                         </div>
                     </div>
                     @empty
-                    @endforelse
+                    @endforelse 
                 </div>
             </div>
         </div>
+        </div><!--jumbotrin-->
     </div>
 
 
