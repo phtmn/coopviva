bbf127478f2ec0a8d00a862405cdde990e7a0a27
Matthaus Nawan de Souza e Silva <matthausnawan@gmail.com>
2019-04-15 11:52:24 -0300

    migracao de projetos

--- database/migrations/2019_04_15_114136_create_field_arquivo_projetos_table.php
+++ database/migrations/2019_04_15_114136_create_field_arquivo_projetos_table.php
@@ -14,6 +14,7 @@ class CreateFieldArquivoProjetosTable extends Migration
     public function up()
     {
         Schema::table('projetos', function (Blueprint $table) {
+            $table->text('arquivo')->nullable();
             //
         });
     }
